729b64545479733d28ac361b0a9a4cd0eb2c6995
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-19 16:19:04 +0300

    Added S3HW6 task: Cubes of numbers with odd last digit

---
+++ S3HW6_CubesOfOddNumbers/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a % 2 == 0) Console.WriteLine($"Куб числа {a} равен {a * a * a}!");
+else Console.WriteLine($"Число {a} не заканчивается на четную цифру!");
