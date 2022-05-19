156c9c1cc51fd534b7946929703cdd66e803a2fb
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-19 15:42:04 +0300

    Added S3HW3 task: Sum from 1 to A

---
+++ S3HW3_SumFrom1ToA/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+for (int i = 1; i <= a; i++) sum = sum + i;
+Console.WriteLine($"Сумма чисел от 1 до {a}: {sum}");
