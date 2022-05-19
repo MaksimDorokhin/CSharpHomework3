4ef2096f64a232107b99f35578a5458dbe844ef3
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-19 15:56:05 +0300

    Added S3HW4 task: Number A in natural degree of B

---
+++ S3HW4_AInDegreeB/Program.cs
@@ -0,0 +1,13 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int result = a;
+if (b <= 0) Console.WriteLine($"Введенное число b не является натуральным!");
+else
+{
+    for (int i = 2; i <= b; i++) result = result * a;
+    Console.WriteLine($"Число {a} в степени {b} = {result}");
+}
