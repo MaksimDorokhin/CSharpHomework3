9e081c94249627fbaa11a4db131d3856e2a37a99
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-19 16:12:46 +0300

    Added S3HW5 task: Sum of digits in number

---
+++ S3HW5_SumOfDigitsInNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int count = 0;
+if (a == 0) Console.WriteLine("Сумма цифр равна 1!");
+else
+{
+    while (a > 0 || a < 0) 
+    {
+        a = a / 10;
+        count = count + 1;
+    }
+    Console.WriteLine($"Сумма цифр равна {count}!");
+}
